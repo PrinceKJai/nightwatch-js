634b22a90f12e1979bdddc64b0d3e25de634797d
Prince Jaiswal <princejaiswal278@gmail.com>
2024-12-06 18:17:24 +0530

    using sections with elements - not working

--- pages/monsterPage.js
+++ pages/monsterPage.js
@@ -11,21 +11,47 @@
 //   },
 // };
 
+// module.exports = {
+//   url: "https://glitchitsystem.com/monster/",
+//   elements: {
+//     usernameLabel: {
+//       selector:
+//         "body > app-root > div > div > div > app-login > div > div > form > div:nth-child(1) > div > div > label",
+//     },
+//     passwordLabel: {
+//       selector:
+//         "body > app-root > div > div > div > app-login > div > div > form > div:nth-child(2) > div > div > label",
+//     },
+//     loginButton: {
+//         selector: "body > app-root > div > div > div > app-login > div > div > form > div:nth-child(3) > div > button",
+//     },
+//     usernameField: "#username",
+//     passwordField: "#password",
+//   },
+// };
+
+//using pages with sections
 module.exports = {
   url: "https://glitchitsystem.com/monster/",
-  elements: {
-    usernameLabel: {
-      selector:
-        "body > app-root > div > div > div > app-login > div > div > form > div:nth-child(1) > div > div > label",
+  sections: {
+    login: {
+      selector: "",
+      elements: {
+        usernameLabel: {
+          selector:
+            "body > app-root > div > div > div > app-login > div > div > form > div:nth-child(1) > div > div > label",
+        },
+        passwordLabel: {
+          selector:
+            "body > app-root > div > div > div > app-login > div > div > form > div:nth-child(2) > div > div > label",
+        },
+        loginButton: {
+          selector:
+            "body > app-root > div > div > div > app-login > div > div > form > div:nth-child(3) > div > button",
+        },
+        usernameField: "#username",
+        passwordField: "#password",
+      },
     },
-    passwordLabel: {
-      selector:
-        "body > app-root > div > div > div > app-login > div > div > form > div:nth-child(2) > div > div > label",
-    },
-    loginButton: {
-        selector: "body > app-root > div > div > div > app-login > div > div > form > div:nth-child(3) > div > button",
-    },
-    usernameField: "#username",
-    passwordField: "#password",
   },
 };
--- test/pageObjectModel/assertions.test.js
+++ test/pageObjectModel/assertions.test.js
@@ -1,8 +1,10 @@
 describe("Assertions Test Suite With Page Object", () => {
   beforeEach((browser) => {
     browser.page.monsterPage().navigate();
+    // browser.url("https://glitchitsystem.com/monster/")
   });
 
+  //without using sections
   xit("should check assertions in the login form", (browser) => {
     const monsterPage = browser.page.monsterPage();
     monsterPage.assert.visible("@usernameLabel", "username label is present");
@@ -11,8 +13,46 @@ describe("Assertions Test Suite With Page Object", () => {
     browser.pause(2000);
   });
 
+  //using sections for the above mentioned test
+  xit("should check assertions in the login form", (browser) => {
+    const monsterPage = browser.page.monsterPage().section.login;
+    monsterPage.assert.visible("@usernameLabel", "username label is present");
+    monsterPage.assert.visible("@passwordLabel", "password label is present");
+    monsterPage.assert.visible("@loginButton", "button is present");
+    browser.pause(2000);
+  });
+
+  //without using sections
   xit("Uses containsText, valueContains and value in the form element", (browser) => {
     const monsterPage = browser.page.monsterPage();
+    monsterPage.assert.textContains(
+      "@usernameLabel",
+      "Username",
+      "Label has Username"
+    );
+    monsterPage.assert.textContains(
+      "@passwordLabel",
+      "Password",
+      "Label has Password"
+    );
+    monsterPage.setValue("@usernameField", "bob@bob.com");
+    monsterPage.assert.valueEquals(
+      "@usernameField",
+      "bob@bob.com",
+      "Username input has value"
+    );
+    monsterPage.setValue("@passwordField", "Test123");
+    monsterPage.assert.valueEquals(
+      "@passwordField",
+      "Test123",
+      "Password input has value"
+    );
+    browser.pause(3000);
+  });
+
+  //using sections for the above mentioned test
+  xit("Uses containsText, valueContains and value in the form element", (browser) => {
+    const monsterPage = browser.page.monsterPage().section.login;
     monsterPage.assert.textContains(
       "@usernameLabel",
       "Username",
